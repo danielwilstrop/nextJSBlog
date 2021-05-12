062abf11080a7060d0abdfbcad84725f6e8f23e1
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-05-12 14:03:01 +0100

    need to just add the MD files

---
+++ pages/about.js
@@ -0,0 +1,54 @@
+import siteMetadata from '@/data/siteMetadata'
+import SocialIcon from '@/components/social-icons'
+import { PageSeo } from '@/components/SEO'
+
+export default function About() {
+  return (
+    <>
+      <PageSeo
+        title={`About - ${siteMetadata.author}`}
+        description={`About me - ${siteMetadata.author}`}
+        url={`${siteMetadata.siteUrl}/about`}
+      />
+      <div className="divide-y">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            About
+          </h1>
+        </div>
+        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+          <div className="flex flex-col items-center pt-8 space-x-2">
+            <img src={siteMetadata.image} alt="avatar" className="w-48 h-48 rounded-full" />
+            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
+              {siteMetadata.author}
+            </h3>
+            <div className="text-gray-500 dark:text-gray-400">Bsc Computing and IT - Student </div>
+            <div className="text-gray-500 dark:text-gray-400">The Open University</div>
+            <div className="flex pt-6 space-x-3">
+              <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
+              <SocialIcon kind="github" href={siteMetadata.github} />
+              <SocialIcon kind="facebook" href={siteMetadata.facebook} />
+            </div>
+          </div>
+          <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
+            <p>
+              Year one student with the Open univeristy, studying computing and IT with a specialism
+              in software develpment.
+            </p>
+            <p>
+              I am currently foccussing on web development and the associated practices, frameworks,
+              languages and packges needed to be sucessful with a strong emphasis on mastering and
+              being fully proficient with javascript.
+            </p>
+            <p>
+              My current skill set includes HTML, CSS, SASS, Javascript, NodeJS/Express, React, the
+              NPM ecosystem and web best practices. I have a good understanding of programming
+              basics such as OOP, functional programming, sort and search alogrithms and DRY
+              priciples.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
